6a4a6043b9020c0be35a7056370b73fa805b12c8
thanosan23 <p.thanosan23@gmail.com>
2023-11-22 13:06:36 -0500

    updated content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,30 @@
 const Footer = () => {
     return (
         <>
-        <footer className="footer p-10">
-        <aside>
-            <img src="/logo.png" alt="logo" className="w-14 h-14"/>
-            <p className="font-bold">Cure for Cause<br/><span className="font-light">Educating the community on disease prevention</span></p>
-        </aside> 
-        <nav>
-            <header className="text-black font-semibold">SOCIAL</header> 
-            <div className="grid grid-flow-col gap-4">
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> 
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
-            </div>
-        </nav>
-        </footer>
+            <footer id="footer" className="footer p-10">
+                <aside>
+                    <img src="/logo.png" alt="logo" className="w-14 h-14"/>
+                    <p className="font-bold">Cure for Cause<br/><span className="font-light">Educating the community on disease prevention.</span></p>
+                </aside>
+                <nav>
+                    <header className="text-black font-semibold">SOCIAL</header>
+                    <div className="grid grid-flow-col gap-4">
+                        <a href="https://instagram.com/cureforcause?igshid=MTk0NTkyODZkYg%3D%3D&utm_source=qr"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 30 30">
+                            <path d="M 9.9980469 3 C 6.1390469 3 3 6.1419531 3 10.001953 L 3 20.001953 C 3 23.860953 6.1419531 27 10.001953 27 L 20.001953 27 C 23.860953 27 27 23.858047 27 19.998047 L 27 9.9980469 C 27 6.1390469 23.858047 3 19.998047 3 L 9.9980469 3 z M 22 7 C 22.552 7 23 7.448 23 8 C 23 8.552 22.552 9 22 9 C 21.448 9 21 8.552 21 8 C 21 7.448 21.448 7 22 7 z M 15 9 C 18.309 9 21 11.691 21 15 C 21 18.309 18.309 21 15 21 C 11.691 21 9 18.309 9 15 C 9 11.691 11.691 9 15 9 z M 15 11 A 4 4 0 0 0 11 15 A 4 4 0 0 0 15 19 A 4 4 0 0 0 19 15 A 4 4 0 0 0 15 11 z"></path>
+                        </svg></a>
+                        <a href="mailto:cureforcause.info@gmail.com">
+                        <svg
+                            viewBox="0 0 900 1000"
+                            fill="currentColor"
+                            height="30"
+                            width="30"
+                        >
+                            <path d="M30 264C8.667 252-.667 238.667 2 224c1.333-9.333 10-14 26-14h846c25.333 0 32 10.667 20 32-5.333 9.333-13.333 16.667-24 22-9.333 4-73.333 38-192 102s-179.333 96.667-182 98c-10.667 6.667-26 10-46 10-18.667 0-34-3.333-46-10-2.667-1.333-63.333-34-182-98S39.333 268 30 264m850 100c13.333-6.667 20-3.333 20 10v368c0 10.667-5.667 21.333-17 32-11.333 10.667-22.333 16-33 16H50c-10.667 0-21.667-5.333-33-16-11.333-10.667-17-21.333-17-32V374c0-13.333 6.667-16.667 20-10l384 200c12 6.667 27.333 10 46 10s34-3.333 46-10l384-200" />
+                        </svg>
+                        </a>
+                    </div>
+                </nav>
+            </footer>
         </>
     )
 }
--- src/components/History.tsx
+++ src/components/History.tsx
@@ -6,13 +6,13 @@ const History = () => {
             <section className="bg-white" id="about">
                 <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
                     <div className="font-light text-gray-500 sm:text-lg">
-                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">We didn{"'"}t reinvent the wheel</h2>
-                        <p className="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
-                        <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
+                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">About Us</h2>
+                        <p className="mb-4">Cure for Cause is a non-profit organization founded by a group of motivated secondary school students based on bringing awareness and education to students regarding issues in global health. </p>
+                        <p className="mb-4">We want to raise awareness on different health conditions impacting the world and how to prevent the proliferation of these issues. </p>
+                        <p className="mb-4">We also plan to raise awareness issues in the healthcare industry with the goal of improving the quality of healthcare and access to healthcare worldwide.</p>
                     </div>
-                    <div className="grid grid-cols-2 gap-4 mt-8">
-                        <img className="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1"/>
-                        <img className="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2"/>
+                    <div className="grid grid-cols-1 gap-4 mt-8">
+                        <img className="w-full rounded-lg" src="https://images.unsplash.com/photo-1576091160550-2173dba999ef?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="office content 1"/>
                     </div>
                 </div>
             </section>

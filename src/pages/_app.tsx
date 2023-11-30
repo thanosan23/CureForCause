1dee996e2fb63568ed6ad0bc575dcd8929075d3f
thanosan23 <p.thanosan23@gmail.com>
2023-11-30 13:17:51 -0500

    added keywords for seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ const App: AppType = ({ Component, pageProps }) => {
             <Head>
                 <title>Cure For Cause</title>
                 <meta name="description" content="Cure for Cause" />
+                <meta name="keywords" content="Cure for Cause,medical,disease,prevention,awareness,nonprofit, disease prevention, cause, cancer, medical issues, disease avoidance, cureforcause, disease education, medical education, medical support, CFC, medical volunteer, Cure For Cause Volunteer, Cure For Cause Events Team Application, CFC Volunteering"/>
                 <link rel="icon" href="icon.png" sizes="any"/>
             </Head>
             <div>
